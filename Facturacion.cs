958f768bf0fb16493d7290e9854468d476da23f9
albertDev <116018402+aam9063@users.noreply.github.com>
2023-10-17 19:15:11 +0200

    Facturacion.cs

--- Facturacion.cs
+++ Facturacion.cs
@@ -1,3 +1,16 @@
+/*
+Una empresa factura a sus clientes el último día de cada mes.
+Si el cliente paga del 1 al 10 del mes siguiente se le hace un descuento de 50 centimos o
+del 10%, el que sea mayor.
+Si paga entre los días 11 y 20 no se le aplica ningún descuento.
+Si paga después del día 20 se le penaliza con 1 euro o el 5%, lo que sea mayor.
+Se desea un programa que lea los datos del cliente: nombre, dirección, CIF y el importe de
+la factura. Tras ello, confeccione una carta dirigida al cliente informándole que tiene una
+factura pendiente de ...€ y lo que deberá pagar según realice el pago del 1 al 10, del 11 al 20
+o después del 20.
+*/
+
+
 using System;
 
 public class Program
